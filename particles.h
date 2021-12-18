2059d11278cd2824f6efdfcb193882c61d03c9e6
MuMuPatrick <patrickpan1436@outlook.com>
2021-12-18 16:25:39 -0500

    Add third dimention to header file

--- particles.h
+++ particles.h
@@ -10,36 +10,36 @@
 
 #include <vector>
 #include "grid.h"
-#include "vec2.h"
+#include "vec3.h"
 
 typedef enum SimulationTypeEnum { PIC = 0, FLIP = 1, APIC = 2 } SimulationType;
 
 struct Particles{
    Grid &grid;
    int np; // number of particles
-   std::vector<Vec2f> x, u; // positions and velocities
+   std::vector<Vec3f> x, u; // positions and velocities
    /* TODO: add helper variables */
-   std::vector<Vec2f> cx, cy; // c vectors stored, times h
+   std::vector<Vec3f> cx, cy, cz; // c vectors stored, times h
 
    // transfer stuff
-   Array2f sum;
+   Array3f sum;
    SimulationType simType;
 
    Particles(Grid &grid_, SimulationType simType_)
       :grid(grid_), np(0),
-       sum(grid_.pressure.nx+1, grid_.pressure.ny+1), simType( simType_ )
+       sum(grid_.pressure.nx+1, grid_.pressure.ny+1, grid_.pressure.nz+1), simType( simType_ )
    {}
 
-   void add_particle(const Vec2f &px, const Vec2f &pu);
+   void add_particle(const Vec3f &px, const Vec3f &pu);
    void transfer_to_grid(void);
    void update_from_grid(void);
    void move_particles_in_grid(float dt);
    void write_to_file(const char *filename_format, ...);
 
    private:
-   template<class T> void accumulate(T &accum, float q, int i, int j, float fx, float fy);
-   template<class T> void affineFix(T &accum, Vec2f c, int i, int j, float fx, float fy);
-   Vec2f computeC(Array2f &ufield, int i, int j, float fx, float fy);
+   template<class T> void accumulate(T &accum, float q, int i, int j, int k, float fx, float fy, float fz);
+   template<class T> void affineFix(T &accum, Vec3f c, int i, int j, int k, float fx, float fy, float fz);
+   Vec3f computeC(Array3f &ufield, int i, int j, int k, float fx, float fy, float fz);
 };
 
 #endif
